80b4cd7663cb612cb036ce6376c322cc61469b1c
Rifat Sarker <rifatswd@gmail.com>
2025-02-10 20:12:23 +0600

    optimize fonts,metadata and import alias

--- src/app/(withCommonLayout)/about/page.js
+++ src/app/(withCommonLayout)/about/page.js
@@ -1,10 +1,14 @@
-import styles from "./about.module.css"
+import styles from "./about.module.css";
+export const metadata = {
+  title: "About",
+  description: "This is about page",
+};
 const AboutPage = () => {
-    return (
-        <div>
-            <h1 className={styles.text_color}>This is about page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className={styles.text_color}>This is about page</h1>
+    </div>
+  );
 };
 
 export default AboutPage;
