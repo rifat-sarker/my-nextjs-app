2babcd584e6c460d0296ba9f8cc6429e01f361bc
Rifat Sarker <rifatswd@gmail.com>
2025-02-11 16:50:23 +0600

    Update product price and enhance data fetching with revalidation

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import ProductCard from "@/components/product/ProductCard";
 
 export default async function HomePage() {
-  const res = await fetch("http://localhost:5000/products");
+  const res = await fetch("http://localhost:5000/products", {
+    next: {
+      revalidate: 1,
+    },
+  });
   const products = await res.json();
-  console.log(products);
+  // console.log(products);
   return (
     <>
-      <h1 className="text-3xl font-bold text-center mt-10">Products</h1>
+      <h1 className="text-3xl font-bold text-center mt-10">Products ab</h1>
       <div className="grid grid-cols-3 gap-4 w-[90%] mx-auto mt-10">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
