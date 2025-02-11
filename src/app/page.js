9d1fab35f537eb82e9dd686463993922b3832867
Rifat Sarker <rifatswd@gmail.com>
2025-02-11 17:08:19 +0600

    advancted data fetching with SSR

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import ProductCard from "@/components/product/ProductCard";
+import Link from "next/link";
 
 export default async function HomePage() {
   const res = await fetch("http://localhost:5000/products", {
@@ -11,10 +12,15 @@ export default async function HomePage() {
   return (
     <>
       <h1 className="text-3xl font-bold text-center mt-10">Products ab</h1>
-      <div className="grid grid-cols-3 gap-4 w-[90%] mx-auto mt-10">
-        {products.map((product) => (
+      <div className="grid grid-cols-3 gap-4 w-[90%] mx-auto my-10">
+        {products.slice(0, 3).map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
+      </div>
+      <div className="text-center">
+        <Link className="bg-black text-white rounded-lg border p-2" href="/products">
+          See more
+        </Link>
       </div>
     </>
   );
---
+++ src/app/products/page.js
@@ -0,0 +1,20 @@
+import ProductCard from "@/components/product/ProductCard";
+
+const AllProductPage = async () => {
+  const res = await fetch("http://localhost:5000/products", {
+    cache: "no-store",
+  });
+  const products = await res.json();
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center mt-10">All Products</h1>
+      <div className="grid grid-cols-3 gap-4 w-[90%] mx-auto mt-10">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AllProductPage;
